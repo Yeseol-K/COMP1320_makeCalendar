0aecc253627c239742bc3d03c338d9434c3be9af
unknown <ye37seol@gmailcom>
2023-09-19 11:00:44 -0700

    third change

--- main.js
+++ main.js
@@ -6,12 +6,8 @@ const month = readline.question("Enter a month");
 const day = readline.question("Enter a day");
 
 //getDayOfTheWeek(1992,"February", 29));
-console.log (getDayOfTheWeek(year, month, day));
-<<<<<<< HEAD
-console.log (makeCalendar(year));
-=======
-console.log (makeCalendar(2019));
->>>>>>> 31d1e3dbcca044d7cf7854508e94b5d95d8c97ee
+console.log(getDayOfTheWeek(year, month, day));
+console.log(makeCalendar(year));
 
 
 
